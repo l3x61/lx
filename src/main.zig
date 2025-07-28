2323739bca0a7053fe60d1f8727a270f7ed62348
l3x61 <l3x61@pm.me>
2025-07-28 02:23:04 +0200

    repl

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,24 @@
 const std = @import("std");
+const allocator = std.heap.c_allocator;
+const print = std.debug.print;
+const eql = std.mem.eql;
+
+const ansi = @import("ansi.zig");
+const Lexer = @import("Lexer.zig");
+const readLine = @import("readline.zig").readline;
 
 pub fn main() !void {
-    std.debug.print("Hello World!\n", .{});
+    while (true) {
+        const line = try readLine(allocator, ansi.cyan ++ "> " ++ ansi.reset);
+        defer allocator.free(line);
+
+        var lexer = try Lexer.init(line);
+        var token = lexer.nextToken();
+        while (token.tag != .eof) : (token = lexer.nextToken()) {
+            if (token.tag == .symbol and eql(u8, token.lexeme, "exit")) return;
+            print(ansi.dimmed ++ "{s}\n" ++ ansi.reset, .{token});
+        }
+    }
 }
 
 test "all" {
---
+++ src/readline.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const span = std.mem.span;
+
+const C = @cImport({
+    @cInclude("stdio.h");
+    @cInclude("stdlib.h");
+    @cInclude("readline/readline.h");
+    @cInclude("readline/history.h");
+});
+
+pub fn readline(allocator: Allocator, prompt: []const u8) ![]u8 {
+    const line = C.readline(prompt.ptr);
+    defer C.free(line);
+
+    if (line == null) return allocator.dupe(u8, "");
+    if (line[0] != 0) _ = C.add_history(line);
+    return allocator.dupe(u8, span(line));
+}
