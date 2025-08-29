4d1ffaa815c2d7399488a17cbbb185fd061ea4c4
l3x61 <l3x61@pm.me>
2025-08-29 16:44:31 +0200

    string type

---
+++ src/String.zig
@@ -0,0 +1,86 @@
+const std = @import("std");
+const testing = std.testing;
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
+const String = @This();
+
+array: ArrayList(u8),
+
+pub const empty = String{ .array = ArrayList(u8).empty };
+
+pub inline fn initSlice(gpa: Allocator, slice: []const u8) !String {
+    var string = String.empty;
+    try string.appendSlice(gpa, slice);
+    return string;
+}
+
+pub inline fn initPrint(
+    gpa: Allocator,
+    comptime fmt: []const u8,
+    args: anytype,
+) !void {
+    var string = String.init();
+    try string.print(gpa, fmt, args);
+    return string;
+}
+
+pub inline fn deinit(self: *String, gpa: Allocator) void {
+    self.array.deinit(gpa);
+}
+
+pub inline fn clone(self: *String, gpa: Allocator) !String {
+    return String.initSlice(gpa, self.getSlice());
+}
+
+pub inline fn getSlice(self: *String) []u8 {
+    return self.array.items;
+}
+
+pub inline fn appendSlice(self: *String, gpa: Allocator, slice: []const u8) !void {
+    return self.array.appendSlice(gpa, slice);
+}
+
+pub inline fn print(self: *String, gpa: Allocator, comptime fmt: []const u8, args: anytype) !void {
+    return self.array.print(gpa, fmt, args);
+}
+
+test "init" {
+    const a = testing.allocator;
+    var string = String.empty;
+    defer string.deinit(a);
+}
+
+test "initSlice" {
+    const a = testing.allocator;
+    const literal = "Hello World!";
+
+    var string = try String.initSlice(a, literal);
+    defer string.deinit(a);
+
+    try testing.expectEqualStrings(string.array.items, literal);
+}
+
+test "initPrint" {
+    const a = testing.allocator;
+    const literal = "Hello World!";
+
+    var string = try String.initSlice(a, literal);
+    defer string.deinit(a);
+
+    try testing.expectEqualStrings(string.getSlice(), literal);
+}
+
+test "clone" {
+    const a = testing.allocator;
+    const literal = "Hello World!";
+
+    var string = try String.initSlice(a, literal);
+    defer string.deinit(a);
+
+    var new = try string.clone(a);
+    defer new.deinit(a);
+
+    try testing.expectEqualStrings(string.getSlice(), literal);
+    try testing.expectEqualStrings(new.getSlice(), literal);
+}
