6894b67fbd1c830e220165e2a4967c138ad8b878
alex <l3x61@pm.me>
2026-04-04 17:51:00 +0200

    use ESC[ as the CSI prefix

--- src/ansi.zig
+++ src/ansi.zig
@@ -5,35 +5,37 @@
 
 // TODO: disable color setting
 
-pub const esc = "\x1B";
-pub const csi = "[";
+pub const escape = "\x1B";
 
-pub const reset = esc ++ csi ++ "0m";
-pub const bold = esc ++ csi ++ "1m";
-pub const dim = esc ++ csi ++ "2m";
-pub const italic = esc ++ csi ++ "3m";
-pub const underline = esc ++ csi ++ "4m";
-pub const blink = esc ++ csi ++ "5m";
-pub const reverse = esc ++ csi ++ "7m";
-pub const hidden = esc ++ csi ++ "8m";
-pub const strikethrough = esc ++ csi ++ "9m";
+/// control_sequence_introducer
+pub const csi = escape ++ "[";
 
-pub const black = esc ++ csi ++ "30m";
-pub const red = esc ++ csi ++ "31m";
-pub const green = esc ++ csi ++ "32m";
-pub const yellow = esc ++ csi ++ "33m";
-pub const blue = esc ++ csi ++ "34m";
-pub const magenta = esc ++ csi ++ "35m";
-pub const cyan = esc ++ csi ++ "36m";
-pub const white = esc ++ csi ++ "37m";
+pub const reset = csi ++ "0m";
+pub const bold = csi ++ "1m";
+pub const dim = csi ++ "2m";
+pub const italic = csi ++ "3m";
+pub const underline = csi ++ "4m";
+pub const blink = csi ++ "5m";
+pub const reverse = csi ++ "7m";
+pub const hidden = csi ++ "8m";
+pub const strikethrough = csi ++ "9m";
 
-pub const erase_to_end = esc ++ csi ++ "0K";
-pub const erase_to_start = esc ++ csi ++ "1K";
-pub const erase_line = esc ++ csi ++ "2K";
+pub const black = csi ++ "30m";
+pub const red = csi ++ "31m";
+pub const green = csi ++ "32m";
+pub const yellow = csi ++ "33m";
+pub const blue = csi ++ "34m";
+pub const magenta = csi ++ "35m";
+pub const cyan = csi ++ "36m";
+pub const white = csi ++ "37m";
 
-pub const move_cursor_left = esc ++ csi ++ "1D";
-pub const move_cursor_right = esc ++ csi ++ "1C";
-pub const move_cursor_up = esc ++ csi ++ "1A";
-pub const move_cursor_down = esc ++ csi ++ "1B";
+pub const erase_to_end = csi ++ "0K";
+pub const erase_to_start = csi ++ "1K";
+pub const erase_line = csi ++ "2K";
 
-pub const get_cursor_position = esc ++ csi ++ "6n";
+pub const move_cursor_left = csi ++ "1D";
+pub const move_cursor_right = csi ++ "1C";
+pub const move_cursor_up = csi ++ "1A";
+pub const move_cursor_down = csi ++ "1B";
+
+pub const get_cursor_position = csi ++ "6n";
